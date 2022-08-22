a8e3db7bb91819a9faed908ef8e2eb2f2ffaea15
aliiiimaher <alimohammadmaher@gmail.com>
2022-08-22 11:28:08 +0430

    vuex and router added

---
+++ src/router.js
@@ -0,0 +1,21 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: "/", redirect: "/coaches" },
+    { path: "/coaches", component: null },
+    {
+      path: "/coaches/:id",
+      component: null,
+      children: [
+        { path: "contact", component: null }, // /coaches/c1/contact
+      ],
+    },
+    { path: "/register", component: null },
+    { path: "/requests", component: null },
+    { path: "/:notFound(.*)", component: null },
+  ],
+});
+
+export default router;
