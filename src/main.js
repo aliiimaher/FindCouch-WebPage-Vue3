5aa741f3df0112a601f7aaa315c39e8428e98a97
aliiiimaher <alimohammadmaher@gmail.com>
2022-08-22 15:34:19 +0430

    card ui added

--- src/main.js
+++ src/main.js
@@ -3,10 +3,13 @@ import { createApp } from 'vue';
 import router from './router.js';
 import store from './store/index';
 import App from './App.vue';
+import BaseCard from './components/ui/BaseCard.vue';
 
 const app = createApp(App)
 
 app.use(router);
 app.use(store);
+
+app.component('base-card', BaseCard);
 
 app.mount('#app');
--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -1,22 +1,24 @@
 <template>
   <section>FILTER</section>
   <section>
-    <div class="controls">
-      <button>Refresh</button>
-      <router-link to="/register">Register as Coach</router-link>
-    </div>
-    <ul v-if="hasCoaches">
-      <coach-item
-        v-for="coach in filteredCoaches"
-        :key="coach.id"
-        :id="coach.id"
-        :first-name="coach.firstName"
-        :last-nam="coach.lastName"
-        :rate="coach.hourlyRate"
-        :areas="coach.areas"
-      ></coach-item>
-    </ul>
-    <h3 v-else>No coach found.</h3>
+    <base-card>
+      <div class="controls">
+        <button>Refresh</button>
+        <router-link to="/register">Register as Coach</router-link>
+      </div>
+      <ul v-if="hasCoaches">
+        <coach-item
+          v-for="coach in filteredCoaches"
+          :key="coach.id"
+          :id="coach.id"
+          :first-name="coach.firstName"
+          :last-nam="coach.lastName"
+          :rate="coach.hourlyRate"
+          :areas="coach.areas"
+        ></coach-item>
+      </ul>
+      <h3 v-else>No coach found.</h3>
+    </base-card>
   </section>
 </template>
 
