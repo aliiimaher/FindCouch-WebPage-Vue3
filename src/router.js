aeaaf0ed301ad9e645c77732cf75cf1cb9d2cad3
aliiiimaher <alimohammadmaher@gmail.com>
2022-08-22 11:47:00 +0430

    pages added

--- src/router.js
+++ src/router.js
@@ -1,20 +1,27 @@
 import { createRouter, createWebHistory } from "vue-router";
 
+import CoachDetail from './pages/coaches/CoachDetail.vue';
+import CoachesList from './pages/coaches/CoachesList.vue';
+import CoachRegistration from './pages/coaches/CoachRegistration.vue';
+import ContactCoach from './pages/requests/ContactCoach.vue';
+import RequestReceived from './pages/requests/RequestReceived.vue';
+import NotFound from './pages/NotFound.vue'; 
+
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: "/", redirect: "/coaches" },
-    { path: "/coaches", component: null },
+    { path: "/coaches", component: CoachesList },
     {
       path: "/coaches/:id",
-      component: null,
+      component: CoachDetail,
       children: [
-        { path: "contact", component: null }, // /coaches/c1/contact
+        { path: "contact", component: ContactCoach }, // /coaches/c1/contact
       ],
     },
-    { path: "/register", component: null },
-    { path: "/requests", component: null },
-    { path: "/:notFound(.*)", component: null },
+    { path: "/register", component: CoachRegistration },
+    { path: "/requests", component: RequestReceived },
+    { path: "/:notFound(.*)", component: NotFound },
   ],
 });
 
